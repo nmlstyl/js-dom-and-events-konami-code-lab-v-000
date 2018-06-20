b3e310fdefb108474b25d5b2f4440e4ae3e0ebe3
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-06-20 01:17:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
-const main = document.getElementById('mocha')
+const input = document.querySelector('input')
 
 function init() {
   // Write your JavaScript code inside the init() function
