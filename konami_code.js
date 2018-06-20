01a9366c3e7eb95ee81c52c53c52a6068004869a
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-06-20 01:33:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,24 +2,25 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 const input = document.querySelector('input')
 
-function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which);
 
-    if (key === code[index]) {
-      index++;
-
-      if (index === code.length) {
-        alert("999999 lives!");
-
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  }
 
 function init() {
   // Write your JavaScript code inside the init() function
+  function onKeyDownHandler(e) {
+      const key = parseInt(e.detail || e.which);
+
+      if (key === code[index]) {
+        index++;
+
+        if (index === code.length) {
+          alert("999999 lives!");
+
+          index = 0;
+        }
+      } else {
+        index = 0;
+      }
+    }
 
   input.addEventListener('keydown', function(e) {
     onKeyDownHandler(e)
