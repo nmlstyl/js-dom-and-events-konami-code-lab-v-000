3ebb4a3b533e73bf07248c45a1da2b62dae92be4
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-06-20 01:43:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
-const input = document.querySelector('input')
+const input = document.querySelector('html')
 
 function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
