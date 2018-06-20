08cea49b64f22537e7c3b96d9a77cf44bf654615
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-06-20 01:35:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
-const input = document.querySelector('input')
+
 
 function init() {
   // Write your JavaScript code inside the init() function
+  const input = document.querySelector('input')
+  
   function onKeyDownHandler(e) {
       const key = parseInt(e.detail || e.which);
 
