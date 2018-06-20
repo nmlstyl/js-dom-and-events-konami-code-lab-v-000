8f8bbc239648cc5952743a73f27d985d4125be6f
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-06-20 01:13:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
-  html.addEventListener('keydown', function(event) {
+  document.addEventListener('keydown', function(event) {
     onKeyDownHandler(event)
   })
 
