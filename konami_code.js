4e48d8c42c07efb12b9edf71a2e11a8132d18643
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-06-20 01:12:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,6 +3,10 @@ let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
+  main.addEventListener('keydown', function(event) {
+    onKeyDownHandler(event)
+  })
+  
 function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
 
@@ -20,7 +24,3 @@ function onKeyDownHandler(e) {
   }
 
 }
-
-main.addEventListener('keydown', function(event) {
-  onKeyDownHandler(event)
-})
