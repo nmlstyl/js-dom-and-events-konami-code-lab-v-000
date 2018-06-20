62f604adba7772706d7bdfd56b069a49d18df699
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-06-20 01:01:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,13 +7,13 @@ function init() {
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
-   
+
     if (key === code[index]) {
       index++;
    
       if (index === code.length) {
         alert("Hurray!");
-   
+
         index = 0;
       }
     } else {
